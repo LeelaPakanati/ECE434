aa364be393107d73c1244e46c39099d4be22b108
Leela Pakanati <leela.pakanati@arm.com>
2019-09-11 19:43:58 -0400

    Change HW1 to use curses.wrapper

--- HW1/etch_a_sketch.py
+++ HW1/etch_a_sketch.py
@@ -2,18 +2,9 @@
 import argparse
 import curses
 
-def main():
-    parser = argparse.ArgumentParser(description='Etch A Sketch configuration parser')
-    parser.add_argument('x_dim', type=int, help='The x dimension of the Etch-A-Sketch')
-    parser.add_argument('y_dim', type=int, help='The x dimension of the Etch-A-Sketch')
-    args = parser.parse_args()
-
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-
-    stdscr.addstr(0,0, "Welcom to Etch-A-Sketch | Contols:\n\tW-A-S-D to move cursor\n\tSpacebar to toggle draw\n\tr to clear the screen\n\tq to quit\n\t")
-    intro_len = 5
+def setup_stage(args, stdscr):
+    stdscr.addstr(0,0, "Welcome to Etch-A-Sketch | Contols:\n\tW-A-S-D to move cursor\n\tr to clear the screen\n\tq to quit\n\t")
+    intro_len = 4
     
     x_axis = "  "
     for i in range(args.x_dim):
@@ -23,25 +14,71 @@ def main():
     for i in range(args.y_dim):
         stdscr.addstr(intro_len+2+i, 0, str(i)+':', curses.A_BOLD) 
 
-    origin = map_coords(0,0)
-    stdscr.addstr(origin[0], origin[1], 'X', curses.A_BLINK) 
-    curpos = (0,0)
+
+def main(stdscr):
+    parser = argparse.ArgumentParser(description='Etch A Sketch configuration parser')
+    parser.add_argument('x_dim', type=int, help='The x dimension of the Etch-A-Sketch')
+    parser.add_argument('y_dim', type=int, help='The x dimension of the Etch-A-Sketch')
+    args = parser.parse_args()
+
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(0)
+    setup_stage(args, stdscr)
+    (currx, curry) = (0,0)
+    (xpos, ypos) = map_coords(currx, curry)
+    stdscr.addstr(xpos, ypos, 'X', curses.A_BLINK) 
     stdscr.refresh()
-    
-    draw_mode = False
+
+
     while True:
         c = stdscr.getkey()
         if c == 'q':
             break
+        if c == 'r':
+            stdscr.clear()
+            setup_stage(args, stdscr)
+            (currx, curry) = (0,0)
+            (xpos, ypos) = map_coords(currx, curry)
+            stdscr.addstr(xpos, ypos, 'X', curses.A_BLINK) 
+            stdscr.refresh()
+
+        elif c == 's':
+            stdscr.addstr(xpos, ypos, 'X')
+            if curry != args.y_dim - 1:
+                curry += 1
+            (xpos, ypos) = map_coords(currx, curry)
+            stdscr.addstr(xpos, ypos, 'X', curses.A_BLINK) 
+            stdscr.refresh()
+        elif c == 'd':
+            stdscr.addstr(xpos, ypos, 'X')
+            if currx != args.x_dim - 1:
+                currx += 1
+            (xpos, ypos) = map_coords(currx, curry)
+            stdscr.addstr(xpos, ypos, 'X', curses.A_BLINK) 
+            stdscr.refresh()
+        elif c == 'w':
+            stdscr.addstr(xpos, ypos, 'X')
+            if curry != 0:
+                curry -= 1
+            (xpos, ypos) = map_coords(currx, curry)
+            stdscr.addstr(xpos, ypos, 'X', curses.A_BLINK) 
+            stdscr.refresh()
+        elif c == 'a':
+            stdscr.addstr(xpos, ypos, 'X')
+            if currx != 0:
+                currx -= 1
+            (xpos, ypos) = map_coords(currx, curry)
+            stdscr.addstr(xpos, ypos, 'X', curses.A_BLINK) 
+            stdscr.refresh()
 
     curses.nocbreak()
     curses.echo()
     curses.endwin()
 
-def map_coords(x, y, introlen=5):
-    real_x = 3 + 2 * x
-    real_y = introlen+ 2 + 2 * y
+def map_coords(x, y, intro_len=4):
+    real_x = intro_len + 2 + y
+    real_y = 3 + 2 * x
     return (real_x, real_y)
 
-if __name__ == '__main__':
-    main()
+curses.wrapper(main)
