40934e07fdf3494ca7c19479e3023875dcc7457a
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-09-19 00:58:00 -0400

    Update Script

--- hw03/alert_temp.py
+++ hw03/alert_temp.py
@@ -7,16 +7,20 @@ import Adafruit_BBIO.GPIO as GPIO
 bus = smbus.SMBus(2)
 address = 0x48
 alert_pin = 'P9_12'
-bus.write_byte_data(address, 3, 26)
-bus.write_byte_data(address, 2, 25)
+bus.write_byte_data(address, 3, 28)
+bus.write_byte_data(address, 2, 27)
+
+def read_temp(ch):
+    print("Alert Triggered")
+    while GPIO.input(alert_pin):
+        temp = bus.read_byte_data(address, 0)
+        print(str(temp) + "C\r", end="")
+        time.sleep(0.25)
+    print("\r\nAlert Restored")
+
 GPIO.setup(alert_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(alert_pin, GPIO.RISING, callback=read_temp)
 
-def read_temp(ch):
-    while GPIO.input(alert_pin):
-        temp = bus.read_byte_data(address, 1)
-        print(str(temp) + "C", end="/r")
-        time.sleep(0.25)
 
 
 while True:
