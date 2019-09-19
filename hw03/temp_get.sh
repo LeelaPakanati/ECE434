cd913674f9f1e26e23e86f1d18ccceb28131a8ed
Leela Pakanati <leela.pakanati@arm.com>
2019-09-19 00:09:36 -0400

    Change temp scrip to do while loop

--- hw03/temp_get.sh
+++ hw03/temp_get.sh
@@ -5,7 +5,9 @@ config-pin -i P9_22
 config-pin P9_21 i2c
 config-pin P9_22 i2c
 
+while true; do
 temp0f=$(($(i2cget -y 2 0x48 00) * 9 / 5 + 32))
 temp1f=$(($(i2cget -y 2 0x49 00) * 9 / 5 + 32))
 
 echo "Sensor0: " $temp0f " | Sensor1: " $temp1f
+done
