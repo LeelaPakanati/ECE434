bfb9320a7280fedc72ba8ad033e31d0d4805ca79
Leela Pakanati <leela.pakanati@arm.com>
2019-09-19 00:49:50 -0400

    Lower threshold

--- hw03/alert_temp.py
+++ hw03/alert_temp.py
@@ -4,19 +4,20 @@ import smbus
 import time
 import Adafruit_BBIO.GPIO as GPIO
 
+bus = smbus.SMBus(2)
+address = 0x48
+alert_pin = 'P9_12'
+bus.write_byte_data(address, 3, 26)
+bus.write_byte_data(address, 2, 25)
+GPIO.setup(alert_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(alert_pin, GPIO.RISING, callback=read_temp)
+
 def read_temp(ch):
     while GPIO.input(alert_pin):
         temp = bus.read_byte_data(address, 1)
         print(str(temp) + "C", end="/r")
         time.sleep(0.25)
 
-bus = smbus.SMBus(2)
-address = 0x48
-alert_pin = 'P9_12'
-bus.write_byte_data(address, 3, 27)
-bus.write_byte_data(address, 2, 26)
-GPIO.setup(alert_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(alert_pin, GPIO.RISING, callback=read_temp)
 
 while True:
     pass
