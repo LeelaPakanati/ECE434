80a6b025115840dfea24d70b00b625c7ef25f9a5
Leela Pakanati <leela.pakanati@arm.com>
2019-09-19 00:42:13 -0400

    Fix script:

--- hw03/alert_temp.py
+++ hw03/alert_temp.py
@@ -6,7 +6,7 @@ import Adafruit_BBIO.GPIO as GPIO
 
 def read_temp(ch):
     while GPIO.input(alert_pin):
-        temp = bus.read_byte_data(address, 0)
+        temp = bus.read_byte_data(address, 1)
         print(str(temp) + "C", end="/r")
         time.sleep(0.25)
 
@@ -18,5 +18,5 @@ bus.write_byte_data(address, 2, 26)
 GPIO.setup(alert_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(alert_pin, GPIO.RISING, callback=read_temp)
 
-while
+while:
     pass
